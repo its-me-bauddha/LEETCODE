80e41327412a1e1a6019bc6f675d83028d78c200
Harsh  Bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2023-12-11 13:38:27 +0530

    Time: 0 ms (100.00%) | Memory: 6.9 MB (80.47%) - LeetSync

--- 415-add-strings/add-strings.cpp
+++ 415-add-strings/add-strings.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
-    string addRE(string& num1,int p1, string& num2,int p2,int carry=0){
-        if(p1 < 0 && p2<0){
+void addRE(string& num1,int p1, string& num2,int p2,int carry, string& ans ){
+        if(p1 < 0 && p2 < 0){
             if(carry != 0){
-                return string(1,carry + '0');
+                ans.push_back(carry + '0' );
             }
-            else{
-                return "";
-            }
+            return;
         }
 
         int n1 = (p1 >= 0 ? num1[p1] : '0') - '0';
@@ -17,15 +15,16 @@ public:
         int digit = csum % 10;
         carry = csum /10;
 
-        string ans = "";
-        ans.push_back(digit + '0');
+       
+        ans.push_back(digit +'0');
 
 
-        ans += addRE(num1,p1-1,num2,p2-1,carry);
-        return ans;
+        addRE(num1,p1-1,num2,p2-1,carry,ans);
+        
     }
     string addStrings(string num1, string num2) {
-        string ans = addRE(num1,num1.size()-1,num2,num2.size()-1);
+        string ans="";
+        addRE(num1,num1.size()-1,num2,num2.size()-1,0,ans);
         reverse(ans.begin(),ans.end());
         return ans;
     }
