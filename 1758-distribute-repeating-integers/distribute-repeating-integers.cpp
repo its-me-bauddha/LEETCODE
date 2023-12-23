c94e3896b26b8a833464a0edf0b1a4e14ed04110
Harsh  Bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2023-12-23 17:17:37 +0530

    Time: 378 ms (72.43%) | Memory: 77.2 MB (52.79%) - LeetSync

---
+++ 1758-distribute-repeating-integers/distribute-repeating-integers.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool canDistributeHelper(vector<int>& counts, vector<int>& quantity ,int ithCustomer){
+
+        if(ithCustomer == quantity.size()){
+            return true;
+        }
+
+        for(int i=0;i<counts.size();i++){
+            if(counts[i] >= quantity[ithCustomer]){
+                counts[i] -= quantity[ithCustomer];
+                if(canDistributeHelper(counts,quantity ,ithCustomer+1)){
+                    return true;
+                }
+                counts[i] += quantity[ithCustomer]; // backtrack
+            }
+        }
+
+        return false;
+    }
+    bool canDistribute(vector<int>& nums, vector<int>& quantity) {
+        unordered_map<int ,int> countMap ;
+        for(auto num : nums){
+            countMap[num]++;
+        }
+        vector<int> counts;
+        for(auto it : countMap){
+            counts.push_back(it.second);
+        }
+        sort(quantity.rbegin(),quantity.rend()); // sort in descreasing order
+        return canDistributeHelper(counts,quantity , 0 );
+
+    }
+};
