bd20c1828c2ebaec8f3bb8859f8bd8d9f1f844f5
Harsh  Bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2024-01-04 11:50:11 +0530

    Time: 205 ms (7.73%) | Memory: 130.9 MB (5.16%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
@@ -11,29 +11,23 @@ public:
             mp[nums[i]]++;
         }
         
-        int count = 0;
+        int minOperation = 0;
         for(int i= 0;i<mp.size();i++){
             if(mp[i] != 0 && mp[i] != 1){
                  if(mp[i] % 3 == 0){
-                        count += mp[i]/3;
+                        minOperation += mp[i]/3;
                         mp[i] = 0;
                 }
-                else if(mp[i] % 3 == 1 ){
-                       count += mp[i]/3 + 1;
+                else if(mp[i] % 3 == 1 || mp[i] % 3 == 2 ){
+                       minOperation += mp[i]/3 + 1;
                         mp[i] = 0;
                 }
-                else if(mp[i] % 3 == 2 ){
-                       count += mp[i]/3 + 1;
-                        mp[i] = 0;
-                }     
+                 
             }
         }
-        //cout << count << " ";
         for(int i= 0;i<mp.size();i++){
            if(mp[i] != 0) return -1;
         }
-        
-
-        return count;
+        return minOperation;
     }
 };
