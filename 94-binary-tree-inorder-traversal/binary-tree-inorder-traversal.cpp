d61cab496d8302360ad5c256d53b32fbd0a6053f
Harsh  Bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2024-01-05 13:00:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (97.14%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -11,15 +11,44 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* root ,vector <int> &ans){
-        if(!root) return ;
-        solve(root->left,ans);
-        ans.push_back(root->val);
-        solve(root->right,ans);
-    }
+        // **************** MORRIS TRAVERSAL *************************// 
+
+        /// T.C = O(n) && S.C  = O(1)
+        // it is simple inoder traversal only difference in morris and inorder is the space complexity :
+        // S.C of indoer. -> O(n). while
+        // S.C of morris  -> O(1)
+        // T.C of both traversal is same i.e  -> O(n)
     vector<int> inorderTraversal(TreeNode* root) {
-        vector <int> ans;
-        solve(root,ans);
+        vector<int> ans;
+        TreeNode * curr = root;
+
+        while(curr ){
+            // left node is NULL , then visit it , and go right
+            if(curr -> left == nullptr){
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+            // left node is NOT NULL
+            else{
+                // find the inorder predecessor
+                TreeNode* pred = curr->left;
+                while(pred ->right != curr && pred ->right){
+                    pred = pred->right;
+                }
+
+                // if right node is null ,then go left after establshing the link from pred to curr
+                if(pred->right == nullptr){
+                    pred->right = curr;
+                    curr = curr->left;
+                }else{
+                    // left is already visited , Go right after visiting curr node, while removing the link
+                    pred ->right = nullptr;
+                    ans.push_back(curr->val);
+                    curr = curr->right;
+                }
+
+            }
+        }
         return ans;
     }
 };
