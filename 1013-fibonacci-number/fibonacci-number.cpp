8dddfcb6720b167f91f4b451de85a35d575a7813
Harsh  Bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2024-01-15 11:13:10 +0530

    Time: 2 ms (75.40%) | Memory: 6.4 MB (26.30%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,12 +1,34 @@
 class Solution {
 public:
+    int recursiveSolve(int n ){
+         // base case 
+        if(n == 1 || n== 0 )
+        return n; 
+
+        return recursiveSolve(n-1) +recursiveSolve(n-2);
+    }
+    int topDownSolve(int n ,vector<int>&dp){
+        // base case 
+        if(n == 1 || n== 0 )
+        return n; 
+
+        // STEP 3 :  Check ans if already exists
+        if(dp[n] != -1)
+        return dp[n];
+
+        // STEP 2 : replace ans with dp[n]
+        dp[n] = topDownSolve(n-1,dp) + topDownSolve(n-2,dp);
+        return dp[n];
+    }
     int fib(int n) {
-        int arr[100];
-        arr[0] = 0 ;
-        arr[1] = 1; 
-        for(int i = 2 ; i <= n ; i++){
-            arr[i] =  arr[i-1] + arr[i-2];
-        } 
-        return arr[n];
+        // 1. recursive solution
+        // int ans = recursiveSolve(n);
+        // return ans;
+
+        // 2. top down approch (momeoization)
+        // STEP 1 : Create  DP array
+        vector<int> dp(n+1,-1);
+        int ans = topDownSolve(n,dp);
+        return ans ;
     }
 };
