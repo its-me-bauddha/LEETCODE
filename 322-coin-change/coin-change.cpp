8affb11a40d497a50bf21b4fb47e2c37bdb79717
Harsh  Bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2024-01-17 16:16:46 +0530

    Time: 102 ms (32.99%) | Memory: 15 MB (51.61%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -39,6 +39,29 @@ public:
         return mini;
         
     }
+    int solveTab(vector<int>&coins,int amount){
+        // Step 1. create dp array & initailize 
+        //dp[i] -> minimum number of coins to create i amount,
+        // that's why  we initialize dp with INT_MAX
+           vector<int> dp(amount+1,INT_MAX);
+
+        // Step 2. observe base case from above
+           dp[0] = 0;
+
+
+        // Step 3. check the range and flow of top down appraoch & code accordingly
+        for(int i = 1 ;i<=amount;i++){
+            for(int j=0;j<coins.size();j++){
+                if((i-coins[j] >= 0) && dp[i- coins[j]] != INT_MAX ){
+                    // replace/ change recursive calls of memoization by dp
+                    int ans = dp[i-coins[j]];
+                    dp[i] = min(dp[i],1 + ans);
+                }
+            }
+        }
+
+        return dp[amount];
+    }
     int coinChange(vector<int>& coins, int amount) {
         // 1 . SOLVE USING RECURSION
         // int ans = solveUsingRecursion(coins,amount);
@@ -47,13 +70,20 @@ public:
         //  else 
         //  return ans; 
 
-        // 2. TOP DOWN APPROACH (Memoization)
+        // // 2. TOP DOWN APPROACH (Memoization)
 
-        //Step 1 : create a dp array
-        vector<int> dp(amount+1,-1);
-        int ans = solveMem(coins,amount,dp);
+        // //Step 1 : create a dp array
+        // vector<int> dp(amount+1,-1);
+        // int ans = solveMem(coins,amount,dp);
+        // if(ans == INT_MAX) return -1;
+        // else 
+        //     return ans;
+
+        // 3. BOTTOM UP APPROACH (Tabulation)
+        int ans = solveTab(coins,amount);
         if(ans == INT_MAX) return -1;
         else 
             return ans;
+
     }
 };
