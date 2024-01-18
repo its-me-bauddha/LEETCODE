c9175749a61faa9f7ac9e1a8b2fb062f2f6347ce
Harsh  Bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2024-01-18 12:09:20 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (8.66%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,12 +1,34 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        int arr[50];
-         arr[0] = 1;
-         arr[1] = 1 ;
-        for(int i = 2 ; i <= n ;i++){
-            arr[i] = arr[i-1] + arr[i-2];
+    int solveMem(int n ,vector<int>&dp){
+        // base case
+        if(n==0 || n==1) return 1;
+        
+        // already exits ans in dp array
+        if(dp[n] != -1 ){
+            return dp[n];
         }
-        return arr[n];
+
+        // function call and store the ans in dp array
+        dp[n] = solveMem(n-1,dp) + solveMem(n-2,dp); 
+
+
+        return  dp[n];
+    }
+    int climbStairs(int n) {
+        // int arr[50];
+        //  arr[0] = 1;
+        //  arr[1] = 1 ;
+        // for(int i = 2 ; i <= n ;i++){
+        //     arr[i] = arr[i-1] + arr[i-2];
+        // }
+        // return arr[n];
+
+
+        // create dp array
+        vector<int> dp(n+1,-1);
+        int ans = solveMem(n,dp);
+
+        return ans;
     }
 };
