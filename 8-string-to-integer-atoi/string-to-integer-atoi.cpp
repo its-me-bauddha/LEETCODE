c7f86ea4aab33d6f748a4d1628c3e7b9048135b6
Harsh  Bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2024-01-20 17:53:38 +0530

    Time: 3 ms (60.93%) | Memory: 8.2 MB (5.36%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        // int integer = atoi (s.c_str()); // C++ stl function which converts string to integer 
+        // // - atio(char*)
+        // return integer;
+
+        int num = 0 ;
+        int i = 0; 
+        int sign = 1 ;  // +ve
+
+        while(s[i] == ' '){
+            i++;
+        }
+
+        if(i<s.size() && (s[i] == '+' || s[i] == '-')){
+            sign = s[i] =='+'? 1: -1;
+            i++;
+        }
+        while(i<s.size() && isdigit(s[i])) {
+            if(num > INT_MAX/10 || (num == INT_MAX/10 && s[i] > '7')){
+                return sign == -1 ? INT_MIN : INT_MAX;
+            }
+            num = num*10 + (s[i] -'0');
+            i++;
+        }
+
+        return num*sign ;
+
+
+    }
+};
