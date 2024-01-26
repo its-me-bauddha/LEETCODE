6fa9d28f753c232cb15beaf0402339bc3dd9c595
Harsh  Bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2024-01-26 12:24:14 +0530

    Time: 152 ms (81.65%) | Memory: 98.2 MB (21.74%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -24,6 +24,26 @@ public:
         dp[index][target] =  include ||exclude;
         return dp[index][target];
     }
+    bool solveUsingTab(vector<int>& nums,int target){
+         int n =nums.size() ;
+        vector<vector<int> >dp(n+1,vector<int>(target+1,0));
+       
+        for(int i=0;i<n;i++){
+            dp[i][0] =  1;
+        }
+
+        for(int index=n-1;index>=0;index--){
+            for(int t = 1;t<=target;t++){
+                 bool include = 0 ;
+                if(t-nums[index] >= 0)
+                  include = dp[index+1][t-nums[index]];
+                bool exclude=  dp[index+1][t];
+                dp[index][t] =  include ||exclude;
+       
+            }
+        }
+         return dp[0][target];
+    }
     bool canPartition(vector<int>& nums) {
         int sum = 0;
         for(auto i : nums){
@@ -35,9 +55,10 @@ public:
         int index = 0;
 
        // bool ans = solveUsingRecursion(nums,target,index);
-       int n = nums.size();
-       vector<vector<int> > dp(n,vector<int>( target+1,-1));
-       bool ans = solveUsingMem(nums,dp,target,index);
+    //    int n = nums.size();
+    //    vector<vector<int> > dp(n,vector<int>( target+1,-1));
+    //    bool ans = solveUsingMem(nums,dp,target,index);
+    bool ans = solveUsingTab(nums,target);
         return ans;
     }
 };
