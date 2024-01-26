850e8d9ba6660a70802da83e22438d1a3272041c
Harsh  Bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2024-01-26 15:30:29 +0530

    Time: 33 ms (28.33%) | Memory: 9.6 MB (14.77%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -38,9 +38,32 @@ public:
         dp[n][target] = ans;
         return dp[n][target];
     }
+    int solveUsingTabulation(int n ,int k ,int target){
+        vector<vector<int>>dp(n+1,vector<int>(target+1 , 0 ));
+        dp[0][0] = 1;
+
+        for(int index = 1 ; index <= n ;index++){
+            for(int t= 1;t<=target;t++){
+                long long int ans = 0 ;
+                for(int i=1;i<=k;i++){
+                    long long int ansRec = 0 ;
+                    if(t- i >=0)
+                       ansRec = dp[index-1][t-i];
+                    ansRec  = ansRec % MOD;
+                    ans  = ans%MOD ;
+                    ans = (ans + ansRec) % MOD;
+                }
+                  dp[index][t] = ans;
+            }
+        }
+        return dp[n][target];
+
+    }
     int numRollsToTarget(int n, int k, int target) {
         // return solveUsingRecursion(n,k,target);
-        vector<vector<long long int>>dp(n+1,vector<long long int>(target+1,-1));
-        return solveUsingMem(n,k,target,dp);
+        // vector<vector<long long int>>dp(n+1,vector<long long int>(target+1,-1));
+        // return solveUsingMem(n,k,target,dp);
+
+        return solveUsingTabulation(n,k,target);
     }
 };
