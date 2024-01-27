9ee697c524135e4e5cb9b106a2f91c43a63783c5
Harsh  Bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2024-01-27 11:24:46 +0530

    Time: 40 ms (17.34%) | Memory: 13.3 MB (13.29%) - LeetSync

--- 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.cpp
+++ 1228-minimum-cost-tree-from-leaf-values/minimum-cost-tree-from-leaf-values.cpp
@@ -29,6 +29,28 @@ public:
         dp[left][right] = ans;
         return dp[left][right];
     }
+    int solveUsingTab(vector<int>arr,map<pair<int,int>,int> &maxi){
+        int n = arr.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int left = n-1;left >=0;left--){
+            for(int right = 0;right <= n-1;right++){
+                if(left >= right) continue;
+                else{
+                    int ans= INT_MAX;
+                    for(int i = left ;i<right;i++){
+                    ans = min(ans,
+                    maxi[{left,i}] * maxi[{i+1 ,right}] +
+                    dp[left][i] +
+                    dp[i+1][right]);
+        
+                     }
+                    dp[left][right] = ans;
+                    
+                }
+            }
+        }
+        return dp[0][n-1];
+    }
     int mctFromLeafValues(vector<int>& arr) {
       map < pair <int,int > , int> maxi;
 
@@ -43,8 +65,9 @@ public:
 
         int n = arr.size();
         // int ans = solveUsingRecursion(arr,maxi,0,n-1);
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        int ans = solveUsingMem(arr,maxi,0,n-1,dp);
+        // vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        // int ans = solveUsingMem(arr,maxi,0,n-1,dp);
+        int ans = solveUsingTab(arr,maxi);
         return ans;
     }
 };
