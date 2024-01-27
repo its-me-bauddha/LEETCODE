4d44acb38bb1a9831dd8e2cb5e48b2d5945b5129
Harsh  Bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2024-01-27 09:53:56 +0530

    Time: 44 ms (90.66%) | Memory: 9.5 MB (10.19%) - LeetSync

--- 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
@@ -25,10 +25,30 @@ public:
 
 
     }
+    int solveUsingTabulation(int n){
+       vector<vector<int>> dp(n+2,vector<int>(n+2,0));
+
+       for(int start = n ; start >=1 ; start--){
+           for(int end = 1 ;end <= n ;end++){
+               if(start >= end) continue;
+               else{
+                   int ans = INT_MAX;
+                   for(int i=start ;i<=end;i++){
+                   ans = min(ans,i + max(dp[start][i-1],dp[i+1][end]));
+                   }
+                  dp[start][end] = ans;
+        
+               }
+           }
+       }
+       return dp[1][n];
+    }
     int getMoneyAmount(int n) {
         // int ans = solveUsingRecursion(1,n);
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        int ans = solveUsingMem(1,n,dp);
+        // vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        // int ans = solveUsingMem(1,n,dp);
+
+        int ans = solveUsingTabulation(n);
         return ans;
     }
 };
