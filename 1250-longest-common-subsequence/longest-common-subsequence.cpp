0187d36dd14cea7faf7b5ced165d6c9135369679
Harsh  Bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2024-01-28 12:27:10 +0530

    Time: 24 ms (58.46%) | Memory: 25.7 MB (31.65%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -31,14 +31,35 @@ public:
        return dp[i][j];
     }
 
+     int longestCS_solveTab(string &text1, string &text2){
+        int n = text1.length();
+        int m = text2.length();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                int ans = 0;
+                if(text1[i] == text2[j])
+                ans = 1 + dp[i+1][j+1];
+                else{
+                    ans = max(dp[i+1][j],dp[i][j+1]);
+                }
+                dp[i][j] = ans;
+            }
+        }
+        return dp[0][0];
+        
+    }
+
     int longestCommonSubsequence(string text1, string text2) {
        
         int i = 0; int j = 0 ;
-        int n = text1.length();
-        int m = text2.length();
-        //  return longestCS_helperRecursion(text1,text2,i,j);
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-       
-        return longestCS_solveMem(text1,text2,i,j,dp);
+        // int n = text1.length();
+        // int m = text2.length();
+        // //  return longestCS_helperRecursion(text1,text2,i,j);
+        // vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        // return longestCS_solveMem(text1,text2,i,j,dp);
+
+        return longestCS_solveTab(text1,text2);
     }
 };
