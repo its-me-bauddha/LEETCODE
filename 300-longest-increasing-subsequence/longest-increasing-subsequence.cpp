7e4f51baa74c4b13b7782c8785d5cb587353a353
Harsh  Bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2024-01-30 12:27:11 +0530

    Time: 183 ms (59.62%) | Memory: 13.1 MB (22.77%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -50,13 +50,37 @@ public:
             return dp[0][0];
         }
 
+    int lengthOfLISUsingSO(vector<int>& nums){
+            int n = nums.size();
+            vector<int> currRow(n+1,0);
+            vector<int> nextRow(n+1,0);
+
+            for(int curr =n - 1;curr >=0 ;curr--){
+                for(int prev =curr-1; prev >= -1 ;prev-- ){
+                    int include = 0 ;
+                    if(prev == -1  ||  nums[prev] < nums[curr]){
+                    include = 1  +  nextRow[curr+1];
+                   }            
+                   int  exclude =   nextRow[prev+1];
+
+                    int ans =  max(include ,exclude);
+                    currRow[prev+1] = ans ;
+           
+                }
+
+                // shift
+                nextRow = currRow;
+            }
+            return nextRow[0];
+        }
+
     int lengthOfLIS(vector<int>& nums) {
             int curr = 0;
             int prev = -1;
 
             // int ans =  lengthOfLISUsingRecursion(nums,curr,prev);
            
-              int ans =  lengthOfLISUsingTab(nums);
+              int ans =  lengthOfLISUsingSO(nums);
             return ans;     
     }
 };
