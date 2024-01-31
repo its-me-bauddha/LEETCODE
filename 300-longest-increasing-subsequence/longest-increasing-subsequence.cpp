98651b8001e7403eac091694a84d65cbb8f0f844
Harsh  Bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2024-01-31 19:26:19 +0530

    Time: 5 ms (88.57%) | Memory: 12.7 MB (26.30%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -74,13 +74,35 @@ public:
             return nextRow[0];
         }
 
-    int lengthOfLIS(vector<int>& nums) {
-            int curr = 0;
-            int prev = -1;
+    int solveOptimal(vector<int>&nums){
+        if(nums.size() == 0 ) return 0;
 
-            // int ans =  lengthOfLISUsingRecursion(nums,curr,prev);
+        vector<int>ans;
+        ans.push_back(nums[0]);
+
+        for(int i=1;i<nums.size();i++){
+            if(ans.back() < nums[i]){
+                ans.push_back(nums[i]);
+            }
+            else{
+                // overwrite 
+                // find the index of just large num 
+                int index = lower_bound(ans.begin(),ans.end(),nums[i]) - ans.begin();
+                ans[index] = nums[i];
+            }
+        }
+        return ans.size();
+    }
+
+    int lengthOfLIS(vector<int>& nums) {
+            // int curr = 0;
+            // int prev = -1;
+
+            // // int ans =  lengthOfLISUsingRecursion(nums,curr,prev);
            
-              int ans =  lengthOfLISUsingSO(nums);
-            return ans;     
+            //   int ans =  lengthOfLISUsingSO(nums);
+            // return ans;   
+
+            return solveOptimal(nums);  
     }
 };
